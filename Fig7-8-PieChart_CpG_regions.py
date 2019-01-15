1c2c15fff009d7c37d853f85ecc8bf9386c50770
Lingxiao He <45273746+LXHe@users.noreply.github.com>
2019-01-15 13:28:14 +0100

    Update and rename Fig6-7-PieChart_CpG_regions.py to Fig7-8-PieChart_CpG_regions.py

--- Fig7-8-PieChart_CpG_regions.py
+++ Fig7-8-PieChart_CpG_regions.py
@@ -1,25 +1,23 @@
-#%%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-##ir = pd.read_csv('C:/Users/u0105352/Desktop/Lingxiao/MOVEAGE2015/Initial_RawData/\
-##BC_corrected_methylation_beta/beta_noob_1_drpT_island_dis.csv', index_col='Island_region',header=0)
-sig_ir = pd.read_csv('C:/Users/u0105352/Desktop/Lingxiao/MOVEAGE2015/Partek/Noob_1_drpT/\
-4-p01_S_vs_N/sumdata/p01_S_vs_N_S_isl_dis.csv', index_col='Island_region', header=0)
-methyl = pd.read_csv('C:/Users/u0105352/Desktop/Lingxiao/MOVEAGE2015/Graphs/unadj_sig_1_all_methl.csv', index_col='Island_region', header=0)
+sig_ir = pd.read_csv(r'\sumdata\p01_S_vs_N_S_isl_dis.csv', index_col='Island_region', header=0)
+methyl = pd.read_csv(r'\Graphs\unadj_sig_1_all_methl.csv', index_col='Island_region', header=0)
 
 cpg_dis = pd.merge(methyl, sig_ir, left_index=True, right_index=True, how='outer')
 cpg_dis['Pct_hypo'] = cpg_dis['Hypo']/cpg_dis['Frequency']*100
 cpg_dis['Pct_hyper'] = cpg_dis['Hyper']/cpg_dis['Frequency']*100
 sig_sum = cpg_dis['Frequency'].sum()
 cpg_dis['Pct_sig'] = cpg_dis['Frequency']/sig_sum*100
-#%%
+
+#===============================================================================================
 # Plot pie chart
+#===============================================================================================
 labels = cpg_dis.index
 sizes_sig = cpg_dis['Pct_sig'].values
 colors_sig = ['orange','deepskyblue','tomato','gold','lightgreen','orchid']
-# To explode or not
+# Code for explode chart
 #explode = (0,0,0.04,0,0,0) 
 
 fig, ax1 = plt.subplots()
@@ -30,28 +28,18 @@ ax1.pie(sizes_sig, labels=labels, colors=colors_sig,
 # Create a ring shape
 # Draw a circle with white facecolor and null linewidth
 centre_circle = plt.Circle((0,0),0.5, fc='white',linewidth=0)
-### Get a reference to the current figure
-##fig = plt.gcf()
+
 # Get the current Axes instance on the current figure
 # Add the pre-drawn circle
 ax1.add_artist(centre_circle)
 ax1.set_title('Distribution of dmCpG sites in CpG regions\n', fontsize='large')
 ax1.axis('equal')
-
 plt.show()
-#%%
-fig, ax2 = plt.subplots()
-### Plot dmCpG distribution
-##idx_name = cpg_dis.index
-##N = len(idx_name)
-##ind = np.arange(N)
-##ax2.bar(ind, cpg_dis['Pct_hyper'], label='Hyper', width=0.3)
-### Create a stacked barChart by specifying the bottom
-##ax2.bar(ind, cpg_dis['Pct_hypo'], bottom=cpg_dis['Pct_hyper'], label='Hypo', width=0.3)
-##ax2.set_xticks(ind)
-##ax2.set_xticklabels(idx_name)
-##ax2.legend(loc='best')
 
+#======================================================================================
+# Barplot
+#======================================================================================
+fig, ax2 = plt.subplots()
 # Plot dmCpG distribution with 0
 idx_name = cpg_dis.index
 N = len(idx_name)/2
@@ -88,23 +76,5 @@ def autolabel(bar):
 autolabel(hyper_bar)
 autolabel(hypo_bar)
 
-##fig.tight_layout()
 plt.show()
-
-
-
-
-### Explode section with max value
-### Target array ARRAY
-### Get the index of the max value from ARRAY (1D)
-##idx = np.argmax(ARRAY)
-### If ARRAY is 2D, then specify axis will return an array index
-### Then idx = np.argmax(ARRAY, axis=0 as row or 1 as column)
-### Create explode zero array
-##explode = np.zeros(len(ARRAY))
-### Specify the index of explode consistent with that in ARRAY
-### Set the value to 0.1
-##explode[idx] = 0.1
-### An explode code is created
-
 
